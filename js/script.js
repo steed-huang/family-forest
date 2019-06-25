11b759ab6abe032f344a35bfd489545b7ddfbfeb
Steed Huang <steedhuang2002@gmail.com>
2019-06-24 23:09:32 -0400

    gltf loader testing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-var scene, camera, renderer, mesh;
-var meshFloor;
-
-var keyboard = {};
+var scene, camera, renderer, mesh, meshFloor;
 var player = { height: 3, speed: 0.2, turnSpeed: Math.PI * 0.015 };
-var USE_WIREFRAME = false;
+var keyboard = {};
 var drawWidth = window.innerWidth * 0.99;
-var drawHeight = window.innerHeight * 0.99; // It extends past end at full innerHeight?
+var drawHeight = window.innerHeight * 0.99; // It extends past end   at full innerHeight?
+var USE_WIREFRAME = false;
 
 function init() {
   scene = new THREE.Scene();
@@ -15,14 +13,7 @@ function init() {
   renderer.setSize(drawWidth, drawHeight);
   document.body.appendChild(renderer.domElement);
 
-  // Box
-  mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 2, 2),
-    new THREE.MeshNormalMaterial({ color: 0xff4444, wireframe: USE_WIREFRAME })
-  );
-  mesh.position.y += 2; // Move the mesh up 1 meter
-  scene.add(mesh);
-
+  /*
   // Room
   meshRoom = new THREE.Mesh(
     new THREE.BoxGeometry(30, 10, 30, 30, 10, 30),
@@ -31,6 +22,24 @@ function init() {
   meshRoom.position.y += 5 - player.height;
   meshRoom.material.side = THREE.DoubleSide;
   scene.add(meshRoom);
+  */
+
+  // Floor
+  meshFloor = new THREE.Mesh(
+    new THREE.PlaneGeometry(30, 30, 30, 30),
+    new THREE.MeshBasicMaterial({ color: 0x575757, wireframe: USE_WIREFRAME })
+  );
+  meshFloor.position.y -= 1.99;
+  meshFloor.rotation.x -= Math.PI / 2; // Rotate the floor 90 degrees
+  scene.add(meshFloor);
+
+  // Cube
+  mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshNormalMaterial({ color: 0xff4444, wireframe: USE_WIREFRAME })
+  );
+  mesh.position.y += 2; // Move the mesh up 1 meter
+  scene.add(mesh);
 
   // Picture
   var pl = new THREE.TextureLoader();
@@ -45,21 +54,41 @@ function init() {
   pic.rotation.z -= Math.PI;
   scene.add(pic);
 
-  // Floor
-  meshFloor = new THREE.Mesh(
-    new THREE.PlaneGeometry(30, 30, 30, 30),
-    new THREE.MeshPhongMaterial({ color: 0x575757, wireframe: USE_WIREFRAME })
+  // Model
+  var loader = new THREE.GLTFLoader();
+  loader.load(
+    "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Ftest.glb?v=1559756997603",
+    function(gltf) {
+      gltf.scene.scale.set(0.5, 0.5, 0.5);
+      gltf.scene.position.set(-12, -2, 12);
+      scene.add(gltf.scene);
+    }
   );
-  meshFloor.position.y -= 1.99;
-  meshFloor.rotation.x -= Math.PI / 2; // Rotate the floor 90 degrees
-  scene.add(meshFloor);
+  loader.load(
+    "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Faventador.glb?v=1560526795607",
+    function(gltf) {
+      gltf.scene.scale.set(3, 3, 3);
+      gltf.scene.position.set(8, -2.1, 9);
+      scene.add(gltf.scene);
+    }
+  );
 
   // Light
-  var light = new THREE.PointLight(0xffffff, 1, 30, 1);
+  var light = new THREE.PointLight(0xffffff, 2, 30, 1);
   light.position.set(0, 2, 0);
   scene.add(light);
-  var light = new THREE.AmbientLight(0x404040); // soft white light
-  scene.add(light);
+
+  // ambient light
+  var amblight = new THREE.AmbientLight(0x404040, 0.5); // soft white light
+  scene.add(amblight);
+
+  var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+  hemiLight.position.set(0, 300, 0);
+  scene.add(hemiLight);
+
+  var dirLight = new THREE.DirectionalLight(0xffffff, 10);
+  dirLight.position.set(75, 300, -75);
+  scene.add(dirLight);
 
   // Camera
   camera.position.set(0, player.height, -5);
