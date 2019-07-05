6430e4df990db89dc8fec752f87e183fa46a1994
Steed Huang <steedhuang2002@gmail.com>
2019-07-05 14:29:41 -0400

    added basic prismatic function to load mr trees in circle

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 var scene, camera, renderer, tree;
 var drawWidth = window.innerWidth;
 var drawHeight = window.innerHeight;
+var model = {
+  pagetree:
+    "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Ftest.glb?v=1559756997603",
+  tree: "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Flowpolytree.glb",
+  lptree: "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Ftest2.glb",
+  balloon: "https://cdn.glitch.com/2671171a-572f-412a-8934-c57ee91f37bb%2Fballoon.fbx?1557858831042"
+};
 
 function init() {
+  // Three.JS //
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(70, drawWidth / drawHeight, 0.1, 1000);
   renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.setSize(drawWidth, drawHeight);
   document.body.appendChild(renderer.domElement);
 
-  // Models
+  // Model loading
   var loader = new THREE.GLTFLoader();
-  loader.load(
-    "https://cdn.glitch.com/b5469c85-7f52-4fd5-a648-8d70ac85ec20%2Ftest.glb?v=1559756997603",
-    function(gltf) {
-      tree = gltf.scene;
-      tree.scale.set(0.5, 0.5, 0.5);
-      tree.position.set(-10, -5, 12);
-      scene.add(tree);
-    }
-  );
+  loader.load(model.pagetree, function(gltf) {
+    tree = gltf.scene;
+    tree.scale.set(0.5, 0.5, 0.5);
+    tree.position.set(-10, -5, 12);
+    scene.add(tree);
+  });
 
   // Lighting
   var ambLight = new THREE.AmbientLight(0x404040, 1); // soft white light
@@ -42,6 +47,19 @@ function init() {
   camera.lookAt(new THREE.Vector3(0, 3, 0));
 
   animate();
+
+  // Prismatic //
+  let theta = 0;
+  let delta_t = 0;
+  let x = 0 + 5 * Math.cos(theta);
+  let z = 3 + 5 * Math.sin(theta);
+  console.log(delta_t);
+  for (let i = 0; i < 8; i++) {
+    theta += i * (Math.PI / 4);
+    x = Math.round(0 + 5 * Math.cos(theta));
+    z = Math.round(3 + 5 * Math.sin(theta));
+    loadModel(model.lptree, i.toString(), 4000, 2000, x, -2, z);
+  }
 }
 
 function animate() {
@@ -50,6 +68,51 @@ function animate() {
     tree.rotation.y += 0.01;
   }
   renderer.render(scene, camera);
+}
+
+function loadModel(src, id, height, width, x, y, z) {
+  let model = document.createElement("ml-model");
+  model.setAttribute("src", src);
+  model.setAttribute("id", id);
+
+  let xpos = (50 * x + 50).toString();
+  let ypos = (50 * y + 50).toString();
+  let zpos = (500 * z).toString();
+
+  let style =
+    "width: " +
+    width +
+    "px; " +
+    "height: " +
+    height +
+    "px; " +
+    "position: absolute; " +
+    "left: " +
+    xpos +
+    "%; " +
+    "top: " +
+    ypos +
+    "%; " +
+    "transform: translate(-50%, -50%);";
+
+  model.setAttribute("style", style);
+  model.setAttribute("z-offset", zpos);
+  model.setAttribute("unbounded", "true");
+  model.setAttribute("raycast", true);
+
+  /*
+  model.addEventListener("node-raycast", function(e) {
+    if (e.detail.inputType === "control") {
+      if (e.detail.type === "nodeOnControlExit") {
+        model.visibility = "visible";
+      } else {
+        model.visibility = "hidden";
+      }
+    }
+  });
+  */
+
+  document.body.appendChild(model);
 }
 
 window.onload = init;
